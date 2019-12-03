acb0b1183170e079827a9cb207c20f53a808f0cb
Ricardo Rosa <rmsrosa@gmail.com>
2019-12-03 12:13:14 -0200

    add to documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,6 +38,12 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
 
 # -- Options for HTML output -------------------------------------------------
 
