187a310962be86df1a05b8e8f0d9d44a51f5431b
Ricardo Rosa <rmsrosa@gmail.com>
2019-12-09 22:38:33 -0200

    add __all__ in __init__.py to restrict import *

--- nbbinder/__init__.py
+++ nbbinder/__init__.py
@@ -3,6 +3,17 @@
 """
 Gives a navigable book-like structure to a collection of Jupyter notebooks.
 """
+__all__ = ['restructure', 'add_contents', 'add_headers', 'add_navigators',
+    'bind']
+    
 from .nbbinder import *
+
+__author__ = nbbinder.__author__
+__homepage__ = nbbinder.__homepage__
+__copyright__ = nbbinder.__copyright__
+__license__ = nbbinder.__license__
+__version__ = nbbinder.__version__
+__mantainer__ = nbbinder.__mantainer__
+__email__ = nbbinder.__email__
 
 name = 'nbbinder'
