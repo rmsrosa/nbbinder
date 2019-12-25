471e5f91a1b5af69f0b4aba41a5aca81e93d0b69
Ricardo Rosa <rmsrosa@gmail.com>
2019-12-25 08:27:17 -0200

    fix version number

--- nbbinder.py
+++ nbbinder.py
@@ -9,7 +9,7 @@ __copyright__ = """Original work Copyright (c) 2016 Jacob VanderPlas
 Modified work Copyright (c) 2019 Ricardo M S Rosa
 """
 __license__ = "MIT"
-__version__ = "0.8a1"
+__version__ = "0.7a4"
 
 import os
 import re
